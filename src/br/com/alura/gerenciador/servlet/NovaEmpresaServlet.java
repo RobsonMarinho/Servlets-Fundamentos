ee4cf3b548fad2a49f57f0eb3b32258e34a78880
Robson <robson _luz15@hotmail.com>
2018-10-15 18:27:06 -0300

    Cria banco e adiciona empresa

--- src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -20,7 +20,15 @@ public class NovaEmpresaServlet extends HttpServlet {
        
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
+		
 		String nomeEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa();	//Criando empresa
+		empresa.setNome(nomeEmpresa);	//get nome da empresa
+		
+		//Criando um banco
+		Banco banco = new Banco();
+		banco.adiciona(empresa);	//Adiciona a empresa no banco!
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
 	
