92d3d8ffd44b9d845bbe0963369d5816c6648735
Robson <robson _luz15@hotmail.com>
2018-10-15 23:00:23 -0300

    JSP para listar empresas

---
+++ WebContent/listaEmpresas.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ page import="java.util.List, br.com.alura.gerenciador.servlet.Empresa" %>	<!-- Importa  -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	Lista  de empresas: <br/>
+	<ul>
+	<% 
+		List<Empresa> lista = (List<Empresa>)request.getAttribute("empresas");
+		for (Empresa empresa : lista) {
+	%>	<!-- Imprime o nome da empresa -->
+		<li><%= empresa.getNome() %></li>	
+	<% //Realiza um laço
+		}
+	%>
+	</ul>
+
+</body>
+</html>
--- src/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
+++ src/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -4,15 +4,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ListaEmpresasServlet
- */
 @WebServlet("/listaEmpresas")
 public class ListaEmpresasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;       
@@ -22,16 +20,10 @@ public class ListaEmpresasServlet extends HttpServlet {
 		
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();	//lista empresas cadastradas
+		request.setAttribute("empresas", lista);
 		
-		PrintWriter out = response.getWriter();	//Insere html no navegador
-		//estrutura html
-		out.println("<html><body>");
-		out.println("<ul>");
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		out.println("</ul>");
-		out.println("</body></html>");
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response);
 	}
 
 }
